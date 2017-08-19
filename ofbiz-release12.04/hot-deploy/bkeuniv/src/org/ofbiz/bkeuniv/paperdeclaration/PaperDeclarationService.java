48637cdbedb19eada40e324818b3ae76d262fb87
dungkhmt <dungkhmt@gmail.com>
2017-08-19 16:44:33 +0700

    paper declaration

--- ofbiz-release12.04/hot-deploy/bkeuniv/src/org/ofbiz/bkeuniv/paperdeclaration/PaperDeclarationService.java
+++ ofbiz-release12.04/hot-deploy/bkeuniv/src/org/ofbiz/bkeuniv/paperdeclaration/PaperDeclarationService.java
@@ -17,7 +17,12 @@ public class PaperDeclarationService {
 	
 	public static Map<String, Object> getPapersOfStaff(DispatchContext ctx, Map<String, ? extends Object> context){
 		Map<String, Object> retSucc = ServiceUtil.returnSuccess();
-		String staffId = (String)context.get("authorStaffId");
+		//String staffId = (String)context.get("authorStaffId");
+		
+		Map<String, Object> userLogin = (Map<String, Object>)context.get("userLogin");
+		//String userLoginId = (String)context.get("userId");//(String)userLogin.get("userLoginId");
+		String staffId = (String)userLogin.get("userLoginId");
+		
 		Debug.log(module + "::getPapersOfStaff, authorStaffId = " + staffId);
 		Delegator delegator = ctx.getDelegator();
 		try{
@@ -28,6 +33,39 @@ public class PaperDeclarationService {
 				Debug.log(module + "::getPapersOfStaff, paper " + gv.get("paperName"));
 			}
 		
+			retSucc.put("papers", papers);
+			
+		}catch(Exception ex){
+			ex.printStackTrace();
+			ServiceUtil.returnError(ex.getMessage());
+		}
+		return retSucc;
+	}
+	public static Map<String, Object> updatePaper(DispatchContext ctx, Map<String, ? extends Object> context){
+		Map<String, Object> retSucc = ServiceUtil.returnSuccess();
+		//String staffId = (String)context.get("authorStaffId");
+		
+		Map<String, Object> userLogin = (Map<String, Object>)context.get("userLogin");
+		//String userLoginId = (String)context.get("userId");//(String)userLogin.get("userLoginId");
+		String staffId = (String)userLogin.get("userLoginId");
+		String paperId = (String)context.get("paperId");
+		String paperName = (String)context.get("paperName");
+		
+		Debug.log(module + "::updatePaper, authorStaffId = " + staffId + ", paperId = " + paperId);
+		Delegator delegator = ctx.getDelegator();
+		
+		try{
+			List<GenericValue> papers = delegator.findList("PaperDeclaration", 
+					EntityCondition.makeCondition(EntityCondition.makeCondition("paperId", EntityOperator.EQUALS, paperId)), 
+					null, null, null, false);
+			for(GenericValue gv: papers){
+				Debug.log(module + "::updatePaper, paper " + gv.get("paperName") + ", new Name = " + paperName);
+			}
+			GenericValue p = papers.get(0);
+			p.put("paperName", paperName);
+			
+			delegator.store(p);
+			
 			retSucc.put("papers", papers);
 			
 		}catch(Exception ex){
