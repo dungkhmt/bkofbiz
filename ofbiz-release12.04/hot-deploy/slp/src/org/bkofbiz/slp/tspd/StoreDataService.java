3f07a6da871bcf112135f9824b0c0449ee36b21e
tuandat95cbn <tuandat95cbn@gmail.com>
2017-09-01 15:53:51 +0700

    add lib google map utils

--- ofbiz-release12.04/hot-deploy/slp/src/org/bkofbiz/slp/tspd/StoreDataService.java
+++ ofbiz-release12.04/hot-deploy/slp/src/org/bkofbiz/slp/tspd/StoreDataService.java
@@ -1,11 +1,19 @@
 package org.bkofbiz.slp.tspd;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.ofbiz.service.DispatchContext;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import com.mso.googlemap.direction.DirectionQuery;
+import com.mso.googlemap.model.Point;
 import com.squareup.okhttp.MediaType;
 
 public class StoreDataService {
@@ -13,13 +21,15 @@ public class StoreDataService {
 	public static Map<String, Object> storeDataJson(DispatchContext dctx,
 			Map<String, ? extends Object> context) {
 		String str = (String) context.get("listPoints");
-		JsonParser jsonParser= new JsonParser();
-		JsonArray jsonArray= jsonParser.parse(str).getAsJsonArray();
-		for(int i=0;i<jsonArray.size();i++){
-			jsonArray.get(i).toString();
-		}
+		Type listType = new TypeToken<List<Point>>() {}.getType();
+		Gson gson= new GsonBuilder().setPrettyPrinting().create();
+		List<Point> list = new Gson().fromJson(str, listType);
+		DirectionQuery directionQuery= DirectionQuery.getInstance();
+		Map<String,Object> res= directionQuery.getMatrixDirection(list, 4);
 		return null;
 	}
-	
+	public static void main(String[] args) {
+		
+	}
 
 }
