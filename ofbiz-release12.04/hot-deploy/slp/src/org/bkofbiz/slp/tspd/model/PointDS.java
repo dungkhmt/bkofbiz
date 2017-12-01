8489d148d056a4d9b117917dc8459bc867699067
tuandat95cbn <tuandat95cbn@gmail.com>
2017-12-01 18:59:55 +0700

    change model tspd

--- ofbiz-release12.04/hot-deploy/slp/src/org/bkofbiz/slp/tspd/model/PointDS.java
+++ ofbiz-release12.04/hot-deploy/slp/src/org/bkofbiz/slp/tspd/model/PointDS.java
@@ -3,10 +3,22 @@ package org.bkofbiz.slp.tspd.model;
 import java.io.Serializable;
 
 public class PointDS implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String P_Id;
 	private String P_Lat;
 	private String P_Lng;
 	private String DS_Id;
+	private String P_AllowDrone;
+	
+	public String getP_AllowDrone() {
+		return P_AllowDrone;
+	}
+	public void setP_AllowDrone(String p_AllowDrone) {
+		P_AllowDrone = p_AllowDrone;
+	}
 	public String getP_Id() {
 		return P_Id;
 	}
@@ -31,13 +43,16 @@ public class PointDS implements Serializable{
 	public void setDS_Id(String dS_Id) {
 		DS_Id = dS_Id;
 	}
-	public PointDS(String p_Id, String p_Lat, String p_Lng, String dS_Id) {
+	public PointDS(String p_Id, String p_Lat, String p_Lng, String dS_Id,
+			String p_AllowDrone) {
 		super();
 		P_Id = p_Id;
 		P_Lat = p_Lat;
 		P_Lng = p_Lng;
 		DS_Id = dS_Id;
+		P_AllowDrone = p_AllowDrone;
 	}
+	
 	
 
 }
