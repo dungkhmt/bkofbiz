ccdcd39e5718ab7eb9254a3ff4bd8f68fc14f5d8
Lê Văn Đức <levanduc10021996@gmail.com>
2018-08-09 00:26:00 +0700

    update view list project proposal jury

--- ofbiz-release12.04/hot-deploy/bkeuniv/src/org/ofbiz/bkeuniv/projectproposalsubmission/ProjectProposalSubmissionService.java
+++ ofbiz-release12.04/hot-deploy/bkeuniv/src/org/ofbiz/bkeuniv/projectproposalsubmission/ProjectProposalSubmissionService.java
@@ -10,8 +10,10 @@ import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.sql.Date;
+import java.sql.ResultSet;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.List;
@@ -49,10 +51,13 @@ import org.ofbiz.utils.BKEunivUtils;
 import org.ofbiz.entity.Delegator;
 import org.ofbiz.entity.DelegatorFactory;
 import org.ofbiz.entity.GenericDelegator;
+import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericValue;
 import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityFunction;
 import org.ofbiz.entity.condition.EntityOperator;
 import org.ofbiz.entity.util.EntityFindOptions;
+import org.ofbiz.entity.util.EntityListIterator;
 
 public class ProjectProposalSubmissionService {
 	// public static String STATUS_CREATED = "CREATED";
@@ -2342,6 +2347,51 @@ public class ProjectProposalSubmissionService {
 		}
 		return retSucc;
 
+	}
+	
+	public static Map<String,Object> JQGetListProjectProposalJuries(DispatchContext dpct,Map<String,?extends Object> context) throws GenericEntityException{
+		Delegator delegator = (Delegator) dpct.getDelegator();
+		List<EntityCondition> listAllConditions = new ArrayList<EntityCondition>();
+		EntityCondition filter = (EntityCondition) context.get("filter");
+		List<String> sort = (List<String>) context.get("sort");
+		EntityFindOptions opts = (EntityFindOptions) context.get("opts");
+		Map<String,String[]> parameters = (Map<String,String[]>) context.get("parameters");
+		Map<String,Object> result = FastMap.newInstance();
+		EntityListIterator list = null;
+		try {
+			GenericValue userLogin = (GenericValue) context.get("userLogin");
+			String userLoginId = userLogin.getString("userLoginId");
+			opts = opts != null  ? opts : new EntityFindOptions();
+			opts.setDistinct(true);
+			opts.setResultSetType(ResultSet.TYPE_SCROLL_SENSITIVE);
+			
+			if(parameters.containsKey("q")) {
+				String q = (String)parameters.get("q")[0];
+				String[] searchKeys = {"facultyName", "projectCallName", "juryId", "juryName"}; 
+				
+				List<EntityCondition> condSearch = new ArrayList<EntityCondition>(); 
+				for(String key: searchKeys) {
+					EntityCondition condition = EntityCondition.makeCondition(EntityFunction.UPPER_FIELD(key), EntityOperator.LIKE, EntityFunction.UPPER("%" + q + "%"));
+					condSearch.add(condition);
+				}
+				listAllConditions.add(EntityCondition.makeCondition(condSearch, EntityOperator.OR));
+			}
+			
+			if(filter != null) {
+				listAllConditions.add(filter);				
+			}
+			
+		 	EntityCondition condition = EntityCondition.makeCondition(listAllConditions, EntityOperator.AND);
+			
+			list = delegator.find("JuryView", condition, null, null, sort, opts);
+			
+			result.put("listIterator", list);
+			
+		} catch (Exception e) {
+			Debug.log(e.getMessage());
+			return ServiceUtil.returnError("Error JQGetListProjectProposalJuries");
+		}
+		return result;
 	}
 
 	public static Map<String, Object> getProjectProposalJury(
