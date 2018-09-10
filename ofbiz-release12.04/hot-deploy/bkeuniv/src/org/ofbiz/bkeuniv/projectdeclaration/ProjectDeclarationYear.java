d8dea745e9c10bcf0c9a6c27b5ce68cf894c3ceb
dungkhmt <dungkhmt@gmail.com>
2018-09-11 01:00:22 +0700

    upgrade

--- ofbiz-release12.04/hot-deploy/bkeuniv/src/org/ofbiz/bkeuniv/projectdeclaration/ProjectDeclarationYear.java
+++ ofbiz-release12.04/hot-deploy/bkeuniv/src/org/ofbiz/bkeuniv/projectdeclaration/ProjectDeclarationYear.java
@@ -10,6 +10,7 @@ import javolution.util.FastMap;
 
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.bkeuniv.paperdeclaration.PaperDeclarationUtil;
 import org.ofbiz.bkeuniv.projectproposalsubmission.ProjectProposalSubmissionServiceUtil;
 import org.ofbiz.entity.Delegator;
 import org.ofbiz.entity.GenericValue;
@@ -81,6 +82,91 @@ public class ProjectDeclarationYear {
 		return retSucc;
 	}
 	
+	public static Map<String, Object> getBudgetDeclarationOfProject(DispatchContext ctx, Map<String, ? extends Object> context) {
+		Map<String, Object> retSucc = ServiceUtil.returnSuccess();
+		
+		Map<String, Object> userLogin = (Map<String, Object>)context.get("userLogin");
+		String staffId = (String) userLogin.get("userLoginId");
+		
+		String researchProjectProposalId = (String)context.get("researchProjectProposalId");
+		//System.out.println("staff id: "+staffId);
+		
+		Delegator delegator = ctx.getDelegator();
+		try{
+			List<EntityCondition> condss = FastList.newInstance();
+			List<Map> projectDeclarations = FastList.newInstance();
+			List<EntityCondition> conds = FastList.newInstance();
+			conds.add(EntityCondition.makeCondition("researchProjectProposalId", EntityOperator.EQUALS, researchProjectProposalId));
+			conds.add(EntityCondition.makeCondition("statusId", EntityOperator.EQUALS, ProjectProposalSubmissionServiceUtil.STATUS_PROJECT_RUNNING));
+			
+			List<GenericValue> listProjectDeclaration = delegator.findList("ResearchProjectBudgetDeclarationYearView", 
+					EntityCondition.makeCondition(conds), 
+					null, null, null, false);
+			Debug.log(module + "::getBudgetDeclarationOfProject, list = " + listProjectDeclaration.size());
+			
+			HashSet<String> academicYearIds = new HashSet<String>();
+			for(GenericValue gv : listProjectDeclaration){
+				academicYearIds.add(gv.getString("academicYearId"));
+			}
+				
+			
+			for(GenericValue gv : listProjectDeclaration){
+				Map<String, Object> map = FastMap.newInstance();
+				for(int i=0; i<arrAttBudget.length; i++){
+					//map.put(arrAttBudget[i], gv.getString(arrAttBudget[i]));
+					map.put(arrAttBudget[i], gv.get(arrAttBudget[i]));
+				}
+				Debug.log(module + "::getBudgetDeclarationOfProject, proposal name = " + map.get("researchProjectProposalName"));
+
+				projectDeclarations.add(map);
+			}
+			List<Map> projectDeclarationsFiltered = FastList.newInstance();// do sum group by year
+			Map<String, Map> map = FastMap.newInstance();
+			
+			for(String y: academicYearIds){
+				for(Map<String, Object> m: projectDeclarations){
+					String academicYearId = (String)m.get("academicYearId");
+					if(!y.equals(academicYearId)) continue;
+					if(map.get(academicYearId) == null){
+						projectDeclarationsFiltered.add(m);
+						map.put(academicYearId, m);
+					}else{
+						Map x = map.get(academicYearId);
+						BigDecimal eb = (BigDecimal)x.get("equipmentBudget");
+						BigDecimal mb = (BigDecimal)x.get("managementBudget");
+						BigDecimal ab = (BigDecimal)x.get("allocatedBudgetYear");
+						
+						BigDecimal m_eb = (BigDecimal)m.get("equipmentBudget");
+						BigDecimal m_mb = (BigDecimal)m.get("managementBudget");
+						BigDecimal m_ab = (BigDecimal)m.get("allocatedBudgetYear");
+						
+						eb = eb.add(m_eb);
+						mb = mb.add(m_mb);
+						ab = ab.add(m_ab);
+						
+						x.put("equipmentBudget", eb);
+						x.put("managementBudget", mb);
+						x.put("allocatedBudgetYear", ab);
+					}
+				}
+			}
+			for(Map m: projectDeclarationsFiltered){
+				String academicYearId = (String)m.get("academicYearId");
+				Map<String, Object> m_rate = PaperDeclarationUtil.computeRateKKLOfProject(delegator, researchProjectProposalId, academicYearId);
+				m.put("rate", m_rate.get("rate"));
+				m.put("budget", m_rate.get("budget"));
+			}
+			
+			//retSucc.put("projectDeclarations", projectDeclarations);
+			retSucc.put("projectDeclarations", projectDeclarationsFiltered);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			ServiceUtil.returnError(ex.getMessage());
+		}
+
+		return retSucc;
+	}
+
 	public static Map<String, Object> getProjectParticipationDeclaration(DispatchContext ctx, Map<String, ? extends Object> context) {
 		Map<String, Object> retSucc = ServiceUtil.returnSuccess();
 		
@@ -141,6 +227,105 @@ public class ProjectDeclarationYear {
 
 		return retSucc;
 	}
+	public static Map<String, Object> getParticipationDeclarationOfProject(DispatchContext ctx, Map<String, ? extends Object> context) {
+		Map<String, Object> retSucc = ServiceUtil.returnSuccess();
+		
+		Map<String, Object> userLogin = (Map<String, Object>)context.get("userLogin");
+		String staffId = (String) userLogin.get("userLoginId");
+		//System.out.println("staff id: "+staffId);
+		String researchProjectProposalId = (String)context.get("researchProjectProposalId");
+		Delegator delegator = ctx.getDelegator();
+		try{
+			List<Map> projectDeclarations = FastList.newInstance();
+			
+			
+			// get list declaration that user_login (staffId) is the createStaffId
+			List<EntityCondition> conds = FastList.newInstance();
+			//conds.add(EntityCondition.makeCondition("createStaffId", EntityOperator.EQUALS, staffId));
+			conds.add(EntityCondition.makeCondition("researchProjectProposalId", EntityOperator.EQUALS, researchProjectProposalId));
+			conds.add(EntityCondition.makeCondition("statusId", EntityOperator.EQUALS, ProjectProposalSubmissionServiceUtil.STATUS_PROJECT_RUNNING));
+			List<GenericValue> listProjectDeclaration = delegator.findList("ResearchProjectParticipationDeclarationYearView", 
+					EntityCondition.makeCondition(conds), 
+					null, null, null, false);
+			
+			
+			for(GenericValue gv : listProjectDeclaration){
+				Map<String, Object> map = FastMap.newInstance();
+				for(int i=0; i<arrAttParticipation.length; i++){
+					map.put(arrAttParticipation[i], gv.get(arrAttParticipation[i]));
+				}
+				Debug.log(module + "::getParticipationDeclarationOfProject, proposal name = " + map.get("researchProjectProposalName"));
+
+				projectDeclarations.add(map);
+			}
+			
+			Debug.log(module + "::getParticipationDeclarationOfProject, list = " + listProjectDeclaration.size());
+				
+			HashSet<String> academicYearStaffIds = new HashSet<String>();
+			for(GenericValue gv : listProjectDeclaration){
+				String y = gv.getString("academicYearId");
+				String st = gv.getString("staffId");
+				academicYearStaffIds.add(st + "-" + y);
+				Debug.log(module + "::getParticipationDeclarationOfProject, ADD KEY " + st + "-" + y);
+			}
+			List<Map> projectDeclarationsFiltered = FastList.newInstance();// do sum group by year
+			Map<String, Map> map = FastMap.newInstance();
+			
+			for(Map<String, Object> m: projectDeclarations){
+				String academicYearId = (String)m.get("academicYearId");
+				String stId = (String)m.get("staffId");
+				Map<String, Object> x = null;
+				for(Map<String, Object> y : projectDeclarationsFiltered){
+					String y_academicYearId = (String)y.get("academicYearId");
+					String y_stId = (String)y.get("staffId");
+					if(academicYearId.equals(y_academicYearId) && stId.equals(y_stId)){
+						x = y; break;
+					}
+				}
+				if(x == null){
+					projectDeclarationsFiltered.add(m);
+				}else{
+					long per = (long)x.get("staffParticipationPercentage");
+					long m_per = (long)m.get("staffParticipationPercentage");
+					x.put("staffParticipationPercentage", per + m_per);
+				}
+			}
+			/*
+			for(String ys: academicYearStaffIds){
+				String[] s = ys.split("-");
+				String st = s[0];
+				String y = s[1];
+				Debug.log(module + "::getParticipationDeclarationOfProject, consider ys = " + ys + ", staff = " + st + ", year = " + y);
+				for(Map<String, Object> m: projectDeclarations){
+					String academicYearId = (String)m.get("academicYearId");
+					String stId = (String)m.get("staffId");
+					Debug.log(module + "::getParticipationDeclarationOfProject, scan m academicYearId = " + academicYearId + ", staff = " + stId); 
+					
+					if(!y.equals(academicYearId) || !st.equals(stId)) continue;
+					if(map.get(ys) == null){
+						projectDeclarationsFiltered.add(m);
+						Debug.log(module + "::getParticipationDeclarationOfProject, first time for " + ys + " --> ADD, sz = " + projectDeclarationsFiltered.size());
+						map.put(ys, m);
+					}else{
+						Map x = map.get(ys);
+						long per = (long)x.get("staffParticipationPercentage");
+						long m_per = (long)m.get("staffParticipationPercentage");
+						x.put("staffParticipationPercentage", per + m_per);
+					}
+				}
+			}
+			*/
+			
+			//retSucc.put("projectDeclarations", projectDeclarations);
+			retSucc.put("projectDeclarations", projectDeclarationsFiltered);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			ServiceUtil.returnError(ex.getMessage());
+		}
+
+		return retSucc;
+	}
+
 	public static Map<String, Object> createProjectParticipationDeclaration(DispatchContext ctx, Map<String, ? extends Object> context){
 		Map<String, Object> retSucc = ServiceUtil.returnSuccess();
 		
--- ofbiz-release12.04/hot-deploy/bkeuniv/webapp/bkeuniv/projectdeclaration/detailResearchProjectDeclarationYear.ftl
+++ ofbiz-release12.04/hot-deploy/bkeuniv/webapp/bkeuniv/projectdeclaration/detailResearchProjectDeclarationYear.ftl
@@ -178,7 +178,7 @@
 							</tbody>
 						</table>
 					</div>
-
+					<!--
 					<div class="row inline-box" style="margin-bottom: 0px; position: relative; padding: 10px 16px 10px 16px;">
 						<div style="line-height: 2; font-size: 18px; text-align: center; font-weight: 700;">
 							${uiLabel.WorkingHourProject}
@@ -210,8 +210,8 @@
 							</tbody>
 						</table>
 					</div>
-					
-					
+					-->
+
 					<div class="row inline-box" style="margin-bottom: 0px; position: relative; padding: 10px 16px 10px 16px;">
 						<div style="line-height: 2; font-size: 18px; text-align: center; font-weight: 700;">
 							KINH PHI
@@ -222,10 +222,7 @@
 									<th>Kinh phi thiet bi</th>
 									<th>Kinh phi quan ly</th>
 									<th>Tong kinh phi</th>
-									<th>Tong kinh phi thiet bi & quan ly</th>
-									<th>Chi so khoi luong</th>
 									<th>Nam ke khai</th>
-									
 								</tr>
 							</thead>
 							<tbody>
@@ -243,57 +240,15 @@
 										<td>${p.allocatedBudgetYear}</td>
 									</#if>
 
-									<#if p.budget?exists>
-										<td>${p.budget}</td>
+									<#if p.academicYearId?exists>
+										<td>${p.academicYearId}</td>
 									</#if>
-									<#if p.rate?exists>
-										<td>${p.rate}</td>
-									</#if>
+								</tr>
+							</#list>
+							</tbody>
+						</table>
+					</div>
 										
-									<#if p.academicYearId?exists>
-										<td>${p.academicYearId}</td>
-									</#if>
-								</tr>
-							</#list>
-							</tbody>
-						</table>
-					</div>
-					
-
-					
-<div class="row inline-box" style="margin-bottom: 0px; position: relative; padding: 10px 16px 10px 16px;">
-						<div style="line-height: 2; font-size: 18px; text-align: center; font-weight: 700;">
-							MUC DO DONG GOP CUA THANH VIEN
-						</div>
-						<table>
-							<thead>
-								<tr>
-									<th>Ho ten Thanh vien</th>
-									<th>Muc do dong gop</th>
-									<th>Nam ke khai</th>
-									
-								</tr>
-							</thead>
-							<tbody>
-							<#list resultParticipationDeclaration.projectDeclarations as p>
-								<tr>
-									<#if p.staffName?exists>
-										<td>${p.staffName}</td>
-									</#if>
-									
-									<#if p.staffParticipationPercentage?exists>
-										<td>${p.staffParticipationPercentage}</td>
-									</#if>
-									
-									<#if p.academicYearId?exists>
-										<td>${p.academicYearId}</td>
-									</#if>
-								</tr>
-							</#list>
-							</tbody>
-						</table>
-					</div>
-															
 				</div>
 			</div>
 		</div>
